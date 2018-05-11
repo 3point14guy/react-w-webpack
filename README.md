f58f26255b907823c698cb73a06b1e807e0c96d8
3poitn14guy <nicholas.piaskoski@gmail.com>
2018-05-11 16:53:06 -0400

    finish basic shopping list app

--- README.md
+++ README.md
@@ -1,35 +1,7 @@
-This repo is for me following along with Petr Tichy (Ihatetomatoes) youtube series about Webpack 2. It starts here: <https://www.youtube.com/watch?v=JdGnYNtuEtE>
+This repo is for me following along with Petr Tichy (Ihatetomatoes) youtube series about React. It starts here:
+<https://www.youtube.com/watch?v=OXmpxz_-pBU&list=PLkEZWD8wbltnXlfyhS5qSMTNb26utkOkI>
 
-Right in the first video I had some problems following along becuase I was getting the error: webpack: command not found.  I went to <https://webpack.js.org/guides> and followed there install guide.  I thought I found that the reason the webpack command was not working because there was no script built for it yet in package.json, but I still can't use the command webpack. Wherever you see and hear Petr using webpack, it can be replaced with `./node_modules/.bin/webpack` and this will work, however.
+And builds off of his Webpack series: <https://www.youtube.com/watch?v=JdGnYNtuEtE>
 
-Video 2:  Just before minute 4, Petr sets the path: in webpack.config.js to 'dist'. Version 3.8.1 requires the path be set as `\__dirname + \'dist\'`
 
-Also, in the same file, where the HtmlWebpackPlugin is added, a third option of `filename: 'index.html',` needed to be added for the index.html file to show up in the dist folder.
-
-Video 7: - Multiple templates options and RimRaf: Petr suddenly switches to using 'npm run prod' where he was using 'dev' before.  Moving the index.html file out of the 'dist' folder seems to only work in production version.
-
-Video 8: was about using Pug.  Pug will not be used with this template.
-
-Video 11: There is an edit that needs to be made in the cssProd variable which Petr does not go over.  Change `publicPath: '/dist'` to `publicPath: './'`.  This will allow the background image from .scss to show in production mode.
-
-Also, if you get this error after adding image-webpack-loader: - Error: dyld: Library not loaded: /usr/local/opt/libpng/lib/libpng16.16.dy    lib
-then run `brew install libpng` and that should fix it.
-
-Video 12: In the ExtractTextPlugin, the first `/` should be excluded from the filename.  The correct code block should look like this
-
-`new ExtractTextPlugin({
-    filename: 'css/[name].css',
-    disable: !isProd,
-    allChunks: true
-}),`
-
-Video 13: MOdal and drop down list were not working after updates in prod env.  Needed to '''npm install --save glob-all''' from the CLI, move bootstrap.bundle.js into the src file and make these changes to the config:
-`
- const glob = require('glob-all');
-`
-`
-paths: glob.sync([
-  path.join(__dirname, 'src/*.html'),
-  path.join(__dirname, 'src/*.js')
-]),
-`
+A sepearate Wepack template that includes React will be set up here (not done yet).
--- src/app.js
+++ src/app.js
@@ -1,30 +1,120 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Headline = () => {
-  return <h1 className="title">Welcome to the React World</h1>
-}
 
-const Greeting = (props) => {
-  // return <p>You will love it {props.name}({props.age})!</p>
-  // or better, you could do it like This:
-  const {name, age} = props;
-  return <p>You will love it {name}({age})!</p>
-}
 
 // App is like yield in Rails. it will render all of our JSX
 export class App extends React.Component {
+
+  constructor(props) {
+    // the keyword super is used to call functions on an objects parent
+    super(props);
+    this.state = {
+      buyItems: ['milk', 'bread', 'eggs'],
+      message: ''
+    }
+  }
+
+addItem(e) {
+  e.preventDefault();
+  const {buyItems} = this.state;
+  const newItem = this.newItem.value
+  const isOnTheList = buyItems.includes(newItem);
+
+  if (isOnTheList) {
+    this.setState({
+      message: 'This item is already on the list.'
+    })
+  } else {
+    newItem !== '' && this.setState({
+      buyItems: [...this.state.buyItems, newItem],
+        message: ''
+    })
+  }
+  this.addForm.reset();
+}
+
+removeItem(item){
+  const newBuyItems = this.state.buyItems.filter(buyItem => {
+    return buyItem !== item;
+  });
+
+  this.setState({
+    buyItems: [...newBuyItems]
+  })
+
+  if(newBuyItems.length === 0){
+    this.setState({
+      message: 'No items on your list, add some.'
+    })
+  }
+}
+
+clearAll(){
+
+  this.setState({
+    buyItems: [],
+    message: 'No items on your list, add some.'
+  })
+}
+
   render() {
+    // destructuring
+    const {buyItems, message} = this.state
     return (
-      <div>
-      <Headline />
-      <Greeting name='Thea' age={"25"} />
+      <div className="container">
+        <header>
+          <h1>Shopping List</h1>
+          <form ref={input => this.addForm = input} className="form-inline" onSubmit={(e) => {this.addItem(e)}}>
+            <div className="form-group">
+              <label className="sr-only" htmlFor="newItemInput">Add New Item</label>
+              <input ref={input => this.newItem = input} type="text" placeholder="Bread" className="form-control" id="newInputItem" />
+            </div>
+            <button type="submit" className="btn btn-primary">Add</button>
+          </form>
+        </header>
+        <div className="content">
+          {
+            (message !== '' || buyItems.length === 0) && <p className="message text-danger">{message}</p>
+          }
+          {
+          buyItems.length > 0 &&
+          <table className="table">
+            <caption>Shopping List</caption>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Item</th>
+                <th className="text-right">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                buyItems.map((item, index) => {
+                  return (
+                    <tr key={index}>
+                      <th scope="row">{index+1}</th>
+                      <td>{item}</td>
+                      <td className="text-right">
+                        <button onClick={(e)=> this.removeItem(item)} type="button" className="btn btn-default btn-sm">Remove</button>
+                      </td>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+            <tfoot>
+              <tr>
+                <td colSpan="2">&nbsp;</td>
+                <td className="tex-right">
+                  <button onClick={(e) => this.clearAll()} className="btn btn-default btn-sm">Clear list</button>
+                </td>
+              </tr>
+            </tfoot>
+          </table>
+        }
+        </div>
       </div>
     )
   }
-}
-// propTypes will validate our data propTypes
-Greeting.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
 }
